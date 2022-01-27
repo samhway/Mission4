f3dce0f820fad30f5c47f8145b76503fa348f96d
samhway <83246475+samhway@users.noreply.github.com>
2022-01-26 18:21:15 -0700

    Add project files.

---
+++ Mission4/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext movieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext context)
+        {
+            _logger = logger;
+            movieContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Movies(MovieResponse response)
+        {
+            movieContext.Add(response);
+            movieContext.SaveChanges();
+            return View("confirmation");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission4/Models/MovieContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<MovieResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieResponse>().HasData(
+
+                new MovieResponse
+                {
+                    MovieId = 1,
+                    Category = "Sci-Fi",
+                    Title = "Dune",
+                    Director = "Denis Villaneuve",
+                    Year = 2021,
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Does the book justice"
+                },
+                new MovieResponse
+                {
+                    MovieId = 2,
+                    Category = "Sports",
+                    Title = "Moneyball",
+                    Director = "Bennett Miller",
+                    Year = 2011,
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Jonah Hill's best work"
+                },
+                new MovieResponse
+                {
+                    MovieId = 3,
+                    Category = "Comedy", 
+                    Title = "The Other Guys",
+                    Director = "Adam McKay",
+                    Year = 2010,
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Funny movie"
+                });
+        }
+    }
+}
